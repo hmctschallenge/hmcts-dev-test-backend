c3166275826eb819b6688cce57edd815189ba625
Da-artof <85499672+Da-artof@users.noreply.github.com>
2025-12-08 18:58:50 +0000

    Remove GetMapping and add PostMapping

--- src/main/java/uk/gov/hmcts/reform/dev/controllers/CaseController.java
+++ src/main/java/uk/gov/hmcts/reform/dev/controllers/CaseController.java
@@ -1,21 +1,40 @@
 package uk.gov.hmcts.reform.dev.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.hmcts.reform.dev.models.ExampleCase;
 
-import java.time.LocalDateTime;
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import uk.gov.hmcts.reform.dev.models.Case;
+import uk.gov.hmcts.reform.dev.repositories.CaseRepository;
+
 
 import static org.springframework.http.ResponseEntity.ok;
+import static org.springframework.http.ResponseEntity.badRequest;
 
 @RestController
+@RequestMapping("/cases")
 public class CaseController {
 
-    @GetMapping(value = "/get-example-case", produces = "application/json")
-    public ResponseEntity<ExampleCase> getExampleCase() {
-        return ok(new ExampleCase(1, "ABC12345", "Case Title",
-                                  "Case Description", "Case Status", LocalDateTime.now()
-        ));
+    private final CaseRepository caseRepository;
+
+    public CaseController(CaseRepository caseRepository) {
+        this.caseRepository = caseRepository;
     }
+
+    @PostMapping()
+    public ResponseEntity<Case> createCase(@RequestBody @Valid Case newCase) {
+        try {
+            this.caseRepository.save(newCase);
+            return ok(newCase);
+        } catch (Exception e) {
+            return badRequest().body(null);
+        }
+        
+    }
+
+    
 }
